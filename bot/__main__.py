a68f6a16910b4cd5947e2317f7ccc575d66ca604
Valentin <32tut32@gmail.com>
2023-09-05 22:02:30 +0300

    1.1.1

--- bot/__main__.py
+++ bot/__main__.py
@@ -34,7 +34,7 @@ async def main() -> None:
         "postgresql+asyncpg",
         username=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
-        host="127.0.0.1",
+        host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
         port=os.getenv("DB_PORT")
     )
