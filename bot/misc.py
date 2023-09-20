5a7c577122979611df152a688e94f4686ee0addd
Valentin <32tut32@gmail.com>
2023-09-20 19:05:47 +0300

    aioredis -> redis

--- bot/misc.py
+++ bot/misc.py
@@ -1,3 +1,9 @@
+import os
+
 from redis import Redis
 
-redis = Redis()
+redis = Redis(
+    host=os.getenv("REDIS_HOST"),
+    password=os.getenv("REDIS_PASSWORD"),
+    port=os.getenv("REDIS_PORT"),
+)
